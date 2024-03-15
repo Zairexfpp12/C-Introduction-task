af6ef3f9eb8661bc8d97a4c200f7605381e9ebc4
Eli Semmedli <alishammedofh@gmail.com>
2024-03-15 09:47:40 +0400

    task added

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,62 @@
+﻿
+////1) Bir integer arrayi yaradın, daha sonra istifadəçidən yaşını soruşun,
+////    \əgər istifadəçinin yaşı bu arrayda varsa true əks halda false çap edin
+
+
+//int[] num = { 9, 23, 45, 34, 56 };
+//head:
+//Console.WriteLine("yasinizi daxil edin");
+//string val = Console.ReadLine();
+//bool number = int.TryParse(val, out int age);
+//int count=0;
+//if(number== false)
+//{
+//    goto head;
+//}
+
+//foreach (var item in num)
+//{
+//    if (age == item)
+//    {
+//        count++;
+//    }
+//}
+//if (count == 1)
+//{
+//    Console.WriteLine(true);
+
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+
+
+string[] names = {"aysel", "eli", "ruslan", "hesen" };
+head:
+Console.WriteLine("Telebenin adini  daxil edin");
+string name = Console.ReadLine();
+int count = 0;
+foreach (var item in names)
+{
+    if (name == item)
+    {
+        count++;
+    }
+}
+ if (count >= 1)
+{ 
+          
+                Console.WriteLine("-------------");
+                Console.WriteLine(name);
+}
+else
+{
+                Console.WriteLine("Bu adda telebe yoxdur");
+                goto head;
+}
+              
+
+
+
